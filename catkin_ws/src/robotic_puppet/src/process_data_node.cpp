c275a4b5e870b4ec6f698109c77a282e635a9e4a
dbguettler <69404804+dbguettler@users.noreply.github.com>
2023-04-10 10:42:24 -0400

    Fix two issues in process_data_node
    - Fixes an issue where the node repeatedly advertises a topic to publish on
    -  Fix the initial offset of the rotation to better match the arm

--- catkin_ws/src/robotic_puppet/src/process_data_node.cpp
+++ catkin_ws/src/robotic_puppet/src/process_data_node.cpp
@@ -8,22 +8,25 @@
 void imuDataCallback(const geometry_msgs::Quaternion::ConstPtr &msg);
 void printQuat(tf2::Quaternion *quat);
 
-tf2::Quaternion quat_offset(0, 0, 0, 1);
+tf2::Quaternion quat_offset(0.7071068, 0, 0, 0.7071068);
 tf2::Quaternion current_rotation(0, 0, 0, 1);
 tf2::Quaternion current_calibrated_rotation(0, 0, 0, 1);
 tf2::Quaternion current_uncalibrated_rotation(0, 0, 0, 1);
 tf2::Quaternion last_calibrated_rotation;
 tf2::Quaternion start_quat;
 tf2::Quaternion rotation_quaternion;
-
+ros::Publisher pub;
+using namespace std;
 int main(int argc, char **argv)
 {
+
     // Set the start_quat from euler angles (RPY)
     start_quat.setEuler(180, 0, -30);
 
     // ROS Setup
     ros::init(argc, argv, "process_data");
     ros::NodeHandle handle;
+    pub = handle.advertise<geometry_msgs::Pose>("desired_arm_pose", 10);
 
     ros::Subscriber sub = handle.subscribe("imu_data", 1000, imuDataCallback);
 
@@ -38,15 +41,11 @@ void imuDataCallback(const geometry_msgs::Quaternion::ConstPtr &msg)
     // Convert from the message quaternion to a useable type
     tf2::Quaternion new_rotation;
     tf2::fromMsg(*msg, new_rotation);
-
     current_uncalibrated_rotation = new_rotation;
     new_rotation = quat_offset * new_rotation;
     current_calibrated_rotation = new_rotation;
 
     // printQuat(&current_calibrated_rotation);
-    // publish to desired_arm_pose
-    ros::NodeHandle handle;
-    ros::Publisher pub = handle.advertise<geometry_msgs::Pose>("desired_arm_pose", 10);
 
     geometry_msgs::Pose pose;
     tf2::convert(current_calibrated_rotation, pose.orientation);
